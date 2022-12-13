90a4a1632afa20e9d4dac9d35249b3651f08a4a3
Marcelo Fabiano <marcelofabianov@gmail.com>
2022-12-13 15:31:25 -0300

    feat: new FederativeUnitEnum

---
+++ src/Enum/FederativeUnitEnum.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Cappuccino\Enum;
+
+enum FederativeUnitEnum: string
+{
+    case AC = 'Acre';
+    case AL = 'Alagoas';
+    case AP = 'Amapá';
+    case AM = 'Amazonas';
+    case BA = 'Bahia';
+    case CE = 'Ceará';
+    case DF = 'Distrito Federal';
+    case ES = 'Espírito Santo';
+    case GO = 'Goiás';
+    case MA = 'Maranhão';
+    case MT = 'Mato Grosso';
+    case MS = 'Mato Grosso do Sul';
+    case MG = 'Minas Gerais';
+    case PA = 'Pará';
+    case PB = 'Paraíba';
+}
